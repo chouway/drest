d6092aaf142842520145c6ed4588b7fb816afb1b
zhouyw <ywzhou@onlyou.com>
2018-04-20 10:11:34 +0800

    init: spring boot, mybatis, linquibase,common-mapper; base data (add remove edit get[page]);

---
+++ drest-api/src/main/java/com/base/drest/domain/ParamInfo.java
@@ -0,0 +1,136 @@
+package com.base.drest.domain;
+
+import javax.persistence.*;
+
+@Table(name = "t_drest_param_info")
+public class ParamInfo {
+    @Column(name = "id")
+    private String id;
+
+    /**
+     * 字典码
+     */
+    @Column(name = "code")
+    private String code;
+
+    /**
+     * 字典名称
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * 类别
+     */
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "parent_id")
+    private String parentId;
+
+    /**
+     * 备注
+     */
+    @Column(name = "remark")
+    private String remark;
+
+    /**
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * 获取字典码
+     *
+     * @return code - 字典码
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * 设置字典码
+     *
+     * @param code 字典码
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * 获取字典名称
+     *
+     * @return name - 字典名称
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置字典名称
+     *
+     * @param name 字典名称
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取类别
+     *
+     * @return type - 类别
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * 设置类别
+     *
+     * @param type 类别
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return parent_id
+     */
+    public String getParentId() {
+        return parentId;
+    }
+
+    /**
+     * @param parentId
+     */
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    /**
+     * 获取备注
+     *
+     * @return remark - 备注
+     */
+    public String getRemark() {
+        return remark;
+    }
+
+    /**
+     * 设置备注
+     *
+     * @param remark 备注
+     */
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
---
+++ drest-service/src/main/java/com/base/drest/dao/ParamInfoDao.java
@@ -0,0 +1,19 @@
+package com.base.drest.dao;
+
+import com.base.drest.domain.ParamInfo;
+import org.apache.ibatis.annotations.Mapper;
+import com.base.framework.service.mybatis.mapper.BaseMapper;
+
+/**
+* 通用 Mapper 代码生成器
+*
+* @author mapper-generator
+*/
+@Mapper
+public interface ParamInfoDao extends BaseMapper<ParamInfo> {
+
+}
+
+
+
+
