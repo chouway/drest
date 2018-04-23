a44bf6f39fa963eee92a682785e5366f6eb810b0
zhouyw <ywzhou@onlyou.com>
2018-04-23 17:34:12 +0800

    add feign eureka ; invoke ok;  cloud 依赖版本 F.M9  同Boot 2.0.1兼容。

--- drest-eureka/src/main/java/com/base/drest/eureka/ApplicationEureka.java
+++ drest-eureka/src/main/java/com/base/drest/eureka/ApplicationEureka.java
@@ -5,16 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
- * EurekaApplication
+ * ApplicationEureka
  * @author zhouyw
  * @date 2018.04.20
  */
 @SpringBootApplication
 @EnableEurekaServer//   声明一个server：@EnableEurekaServer
-public class EurekaApplication
+public class ApplicationEureka
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(ApplicationEureka.class, args);
     }
 }
--- drest-service/src/main/java/com/base/drest/pub/HelloPubInfoService.java
+++ drest-service/src/main/java/com/base/drest/pub/HelloPubInfoService.java
@@ -3,7 +3,10 @@ package com.base.drest.pub;
 import com.base.drest.domain.ParamInfo;
 import com.base.drest.service.common.IParamInfoService;
 import com.base.framework.common.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloPubInfoService implements IHelloPubInfoService {
 
+    private static Logger logger = LoggerFactory.getLogger(HelloPubInfoService.class);
+
+
     @Autowired
     private IParamInfoService paramInfoService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @Override
     public ParamInfo getByTypeCode(String type, String code) throws BusinessException {
+        logger.info("-->serverPort={}", serverPort);
         return paramInfoService.getByTypeCode(type, code);
     }
 }
